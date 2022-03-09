4aae15ff151412876ceac31a7286b73c40e4da20
sajjadpsavoji <savoji@yorku.ca>
2022-03-09 14:52:05 -0500

    preprocessing was done in matlab; file size is huge try to fix that; open cv and MATLAB use different distortion models

--- preprocessing/preprocess.m
+++ preprocessing/preprocess.m
@@ -1,7 +1,7 @@
 GoproInfo = load('/home/savoji/Desktop/TransPlan Project/Dataset/GoProHero8CameraParams.mat');
 params = GoproInfo.GoProHero8cameraParams.Intrinsics;
 v = VideoReader("./../../Dataset/GX010069.MP4");
-w = VideoWriter("./../../Dataset/GX010069_undistorted.MP4");
+w = VideoWriter("./../../Dataset/GX010069_undistorted");
 open(w);
 while(hasFrame(v))
     frame = readFrame(v);
