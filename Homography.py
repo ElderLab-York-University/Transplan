9c0d2fc338cebff032da595133ecee5c2b1e1e90
sajjadpsavoji <savoji@yorku.ca>
2022-05-29 10:57:46 -0400

    reprojection and homography GUI is done

--- Homography.py
+++ Homography.py
@@ -1,5 +1,6 @@
 from Utils import *
 from Libs import *
+from Track import *
 # import homographygui.tabbed_ui_func as tui
 
 #hints the vars set for homography are 
@@ -8,6 +9,7 @@ from Libs import *
     # args.HomographyTXT
     # args.HomographyNPY
     # args.HomographyCSV
+    # args.ReprojectedPoints 
 
 def homographygui(args):
     # assume homography repo is made in results
@@ -33,3 +35,32 @@ def lunch_homographygui(args):
     os.chdir("./homographygui/")
     os.system(f"sudo python3 main.py --StreetView='{street}' --TopView='{top}' --Txt='{txt}' --Npy='{npy}' --Csv='{csv}'")
     os.chdir(cwd)
+
+def reproject(args):
+    homography_path = args.HomographyNPY
+    out_path = args.ReprojectedPoints 
+
+    current_tracker = trackers[args.Tracker]
+    df = current_tracker.df(args)
+    
+    M = np.load(homography_path, allow_pickle=True)[0]
+    with open(out_path, 'w') as out_file:
+        for index, row in tqdm(df.iterrows()):
+            # fn, idd, x, y = track[0], track[1], (track[2] + track[4]/2), (track[3] + track[5])/2
+            fn, idd, x, y = row['fn'], row['id'], row['x2'], (row['y1'] + row['y2'])/2
+            point = np.array([x, y, 1])
+            new_point = M.dot(point)
+            new_point /= new_point[2]
+            print(f'{int(fn)},{int(idd)},{new_point[0]},{new_point[1]}', file=out_file)
+    return SucLog("Homography reprojection executed successfully")   
+
+def reprojected_df(args):
+    in_path = args.ReprojectedPoints 
+    points = np.loadtxt(in_path, delimiter=',')
+    data  = {}
+    data["fn"] = points[:, 0]
+    data["id"] = points[:, 1]
+    data["x"]  = points[:, 2]
+    data["y"]  = points[:, 3]
+    return pd.DataFrame.from_dict(data)
+
--- main.py
+++ main.py
@@ -11,65 +11,73 @@ from Libs import *
 from Utils import *
 from Detect import detect, visdetect
 from Track import track, vistrack
+from Homography import homographygui
+from Homography import reproject
 
 def Preprocess(args):
     if args.Preprocess:
-        print(FailLog("Preprocessing part should be done in MARLAB for now"))
-    else: print(WarningLog("skipped preprocess subtask"))
+        return FailLog("Preprocessing part should be done in MARLAB for now")
+    else: return WarningLog("skipped preprocess subtask")
 
 def Detect(args):
     if args.Detect:
         print(ProcLog("Detection in Process"))
         log = detect(args)
-        print(log)
-    else: print(WarningLog("skipped detection subtask"))
+        return log
+    else: return WarningLog("skipped detection subtask")
 
 def VisDetect(args):
     if args.VisDetect:
         print(ProcLog("Viz-Detection in Process"))
         log = visdetect(args)
-        print(log)
-    else: print(WarningLog("skipped viz-detection subtask"))
+        return log
+    else: return WarningLog("skipped viz-detection subtask")
 
 def Track(args):
     if args.Track:
         print(ProcLog("Tracking in Process"))
         log = track(args)
-        print(log)
-    else: print(WarningLog("skipped tracking subtask"))
+        return log
+    else: return WarningLog("skipped tracking subtask")
 
 def VisTrack(args):
     if args.VisTrack:
         print(ProcLog("Vis-Tracking in Process"))
         log = vistrack(args)
-        print(log)
-    else: print(WarningLog("skipped vis-tracking subtask"))
+        return log
+    else: return WarningLog("skipped vis-tracking subtask")
 
 def HomographyGUI(args):
     if args.HomographyGUI:
-        raise NotImplemented
-    else: print(WarningLog("skipped homography GUI subtask"))
+        print(ProcLog("Homography GUI in Process"))
+        log = homographygui(args)
+        return log
+    else: return WarningLog("skipped homography GUI subtask")
 
 def Homography(args):
     if args.Homography:
-        raise NotImplemented
-    else: print(WarningLog("skipped homography subtask"))
+        print(ProcLog("Homography reprojection in Process"))
+        log = reproject(args)
+        return log
+    else: return WarningLog("skipped homography subtask")
+
 def TrackLabelingGUI(args):
     if args.TrackLabelingGUI:
         raise NotImplemented
-    else: print(WarningLog("skipped track labelling subtask"))
+    else: return WarningLog("skipped track labelling subtask")
 
 def Count(args):
     if args.Count:
         raise NotImplemented
-    else: print(WarningLog("skipped counting subtask"))
+    else: return WarningLog("skipped counting subtask")
 
 def main(args):
     # Pass the args to each subtask
     # Each subtask will validate its own inputs
     subtasks = [Preprocess, Detect, VisDetect, Track, VisTrack, HomographyGUI, Homography, TrackLabelingGUI, Count]
     for subtask in subtasks:
-        subtask(args)
+        log = subtask(args)
+        print(log)
     
 if __name__ == "__main__":
     # ferch the arguments
