0ba06504f36b2090d43dc1aaed570445145810ea
sajjadpsavoji <savoji@yorku.ca>
2023-06-22 18:40:45 -0400

    udating run flow

--- run.py
+++ run.py
@@ -2,26 +2,26 @@ import os
 # choose the dataset/video
 # options : ['./../Dataset/DandasStAtNinthLineFull', './../Dataset/DandasStAtNinthLine', "./../Dataset/SOW_src1", "./../Dataset/SOW_src2", "./../Dataset/SOW_src3", "./../Dataset/SOW_src4"]
 sources = [
-# '/home/savoji/Desktop/TransPlanProject/Dataset/preprocessed/DundasStreetAtNinthLine/2020Dec18/GX010069'
-# "./../Dataset/DandasStAtNinthLineFull"
-# './../Dataset/DandasStAtNinthLine'
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/DundasStreetAtNinthLine/Video1',
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/DundasStreetAtNinthLine/Video2'
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/BronteRoadAtDundasStreet/2020Dec21/Video1',
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/BronteRoadAtDundasStreet/2020Dec21/Video2',
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/DundasStreetAtWinstonParkDrive/Video1',
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/DundasStreetAtWinstonParkDrive/Video2',
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/EglintonAveWAtCreditviewRoad/Video1',
-# '/media/sajjad/4f8a4d31-2741-4ef9-9e0a-fbd03dcf4d28/TransPlanProject/Dataset/preprocessed/EglintonAveWAtCreditviewRoad/Video2',
-"/home/sajjad/Dataset/PreProcessedMain/D9L_Video1",
-# "/home/sajjad/Dataset/PreProcessedMain/D9L_Video2",
+# "/home/sajjad/Dataset/PreProcessedMain/D9L_Video1",
+"/home/sajjad/Dataset/PreProcessedMain/D9L_Video2",
 # "/home/sajjad/Dataset/PreProcessedMain/DBR_Video1",
-# "/home/sajjad/Dataset/PreProcessedMain/DBR_Video2",
+"/home/sajjad/Dataset/PreProcessedMain/DBR_Video2",
 # "/home/sajjad/Dataset/PreProcessedMain/DWP_Video1",
-# "/home/sajjad/Dataset/PreProcessedMain/DWP_Video2",
+"/home/sajjad/Dataset/PreProcessedMain/DWP_Video2",
 # "/home/sajjad/Dataset/PreProcessedMain/ECR_Video1",
-# "/home/sajjad/Dataset/PreProcessedMain/ECR_Video2",
+"/home/sajjad/Dataset/PreProcessedMain/ECR_Video2",
 
+]
+
+cached_cnt_sources = [
+    # "/home/sajjad/Dataset/PreProcessedMain/D9L_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    "/home/sajjad/Dataset/PreProcessedMain/D9L_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    # "/home/sajjad/Dataset/PreProcessedMain/DBR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    "/home/sajjad/Dataset/PreProcessedMain/DBR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    # "/home/sajjad/Dataset/PreProcessedMain/DWP_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    "/home/sajjad/Dataset/PreProcessedMain/DWP_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    # "/home/sajjad/Dataset/PreProcessedMain/ECR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    "/home/sajjad/Dataset/PreProcessedMain/ECR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
 ]
 
 # choose the detectors
@@ -41,8 +41,7 @@ clusters = ["SpectralFull"]
 clt_metrics = ["tcos", "cmm"]
 cnt_metrics = ["kde"]
 
-for src in sources:
-    pass
+for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     ########################################################
     # 1. estimate the Homography Metrix using Homography GUI 
     # os.system(f"python3 main.py --Dataset={src}  --Detector=detectron2 --Tracker=sort --HomographyGUI --VisHomographyGUI")
@@ -53,36 +52,36 @@ for src in sources:
     # 1.5 visualizing the region of interest 
     # os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --VisROI")
     #######################################################
-    # os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --VisROI")
+    os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --VisROI")
 
     ########################################################
     # 2. run the detection
     # the full commonad looks like : os.system(f"python3 main.py --Datas`et={src}  --Detector={det} --Tracker=NULL --Detect --DetPostProc --DetMask --DetTh=0.50 --VisDetect")
     ########################################################
-    # for det in detectors:
-    #     print(f"detecting ----> src:{src} det:{det}")
-    #     os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker=NULL --VisDetect --ForNFrames=1800 --DetPostProc --DetTh=0.50")
+    for det in detectors:
+        print(f"detecting ----> src:{src} det:{det}")
+        os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker=NULL --VisDetect --ForNFrames=1800 --DetPostProc --DetTh=0.50")
 
     ########################################################
     # 3. run the tracking 
     # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Track --VisTrack --ForNFrames=1800 --Homography --Meter --VisTrajectories --VisTrackTop")
     ########################################################
-    # for det in detectors:
-    #     for tra in trackers:
-    #         print(f"tracking ---> src:{src} det:{det} tra:{tra}")
-    #         # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Track --VisTrack --ForNFrames=1800 --Homography --Meter --VisTrajectories --VisTrackTop")
-    #         os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Track --VisTrack --ForNFrames=1800 --Homography --Meter --VisTrajectories --VisTrackTop")
+    for det in detectors:
+        for tra in trackers:
+            print(f"tracking ---> src:{src} det:{det} tra:{tra}")
+            # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Track --VisTrack --ForNFrames=1800 --Homography --Meter --VisTrajectories --VisTrackTop")
+            os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Track --VisTrack --ForNFrames=1800 --Homography --Meter --VisTrajectories --VisTrackTop")
 
     ########################################################
     # 3.1 run the track post processing
-    # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --TrackPostProc --TrackTh=8 --RemoveInvalidTracks --SelectDifEdgeInROI --SelectEndingInROI --SelectBeginInROI --HasPointsInROI --MaskROI  --CrossROI --CrossROIMulti --JustEnterROI --JustExitROI --WithinROI")
+    # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --TrackPostProc --TrackTh=8 --RemoveInvalidTracks --SelectDifEdgeInROI --SelectEndingInROI --SelectBeginInROI --MaskGPFrame --HasPointsInROI --MaskROI  --CrossROI --CrossROIMulti --JustEnterROI --JustExitROI --WithinROI --Interpolate --ExitOrCrossROI")
     ########################################################
     for det in detectors:
         for tra in trackers:
             print(f"tracking ---> src:{src} det:{det} tra:{tra}")
-            os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --TrackPostProc --HasPointsInROI --CrossROIMulti")
+            os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --TrackPostProc  --MaskGPFrame --HasPointsInROI --ExitOrCrossROI")
             #to visualize results
-            # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --VisTrack --ForNFrames=1800 --VisTrajectories --VisTrackTop")
+            os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --VisTrack --ForNFrames=1800 --VisTrajectories --VisTrackTop")
 
     ########################################################
     # 4. run clustering algorithm
@@ -112,13 +111,13 @@ for src in sources:
 
     ########################################################
     # 7. Run the classification(counting) part
-    # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Count --CountMetric={metric} --CountVisPrompt --EvalCount --UseCachedCounter --CachedCounterPth --CacheCounter")
+    # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Count --CountMetric={metric} --CountVisPrompt --EvalCount --UseCachedCounter --CachedCounterPth={cached_cnt_pth} --CacheCounter --CountVisDensity")
     ########################################################
-    # for det in detectors:
-    #     for tra in trackers:
-    #         for metric in cnt_metrics:
-    #             print(f"counting metric:{metric}")
-    #             os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Count --CountMetric={metric} --EvalCount --UseCachedCounter --CachedCounterPth='/home/sajjad/Dataset/PreProcessedMain/ECR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl'")
+    for det in detectors:
+        for tra in trackers:
+            for metric in cnt_metrics:
+                print(f"counting metric:{metric}")
+                os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Count --CountMetric={metric} --EvalCount --UseCachedCounter --CachedCounterPth={cached_cnt_pth}")
 
     ########################################################
     # 8. Visualizing the results on a video including track label and track id
