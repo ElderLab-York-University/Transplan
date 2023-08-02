f1cfc4b6c617e57af020c0801a6ad67b0b080f8a
sajjadpsavoji <savoji@yorku.ca>
2023-08-02 14:42:31 -0400

    clearn up run.py

--- run.py
+++ run.py
@@ -3,12 +3,16 @@ import os
 # options : ['./../Dataset/DandasStAtNinthLineFull', './../Dataset/DandasStAtNinthLine', "./../Dataset/SOW_src1", "./../Dataset/SOW_src2", "./../Dataset/SOW_src3", "./../Dataset/SOW_src4"]
 sources = [
 # TransPlan Dataset
+    # # D9L
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/D9L_Video1",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/D9L_Video2",
+    # # DBR
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DBR_Video1",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DBR_Video2",
+    # # DWP
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DWP_Video1",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DWP_Video2",
+    # # ECR
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/ECR_Video1",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/ECR_Video2",
 # HW7 & Leslie  DATASET
@@ -17,11 +21,11 @@ sources = [
     # "/mnt/data/HW7Leslie/Seg01/Seg01sc2",
     # "/mnt/data/HW7Leslie/Seg01/Seg01sc3",
     # "/mnt/data/HW7Leslie/Seg01/Seg01sc4",
-    # Seg02
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc1",
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc2",
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc3",
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc4",
+    # # Seg02
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc1",
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc2",
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc3",
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc4",
     # # Seg03
     # "/mnt/data/HW7Leslie/Seg03/Seg03sc1",
     # "/mnt/data/HW7Leslie/Seg03/Seg03sc2",
@@ -72,12 +76,16 @@ sources = [
 
 cached_cnt_sources = [
 # TransPlan Dataset
+    # # D9L
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/D9L_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/D9L_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    # # DBR
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DBR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DBR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    # # DWP
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DWP_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/DWP_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
+    # # ECR
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/ECR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
     # "/mnt/data/TransPlanData/Dataset/PreProcessedMain/ECR_Video1/Results/Counting/video.counting.InternImage.ByteTrack.kde.cached.pkl",
 # HW7 & Leslie  DATASET
@@ -86,11 +94,11 @@ cached_cnt_sources = [
     # "/mnt/data/HW7Leslie/Seg01/Seg01sc2",
     # "/mnt/data/HW7Leslie/Seg01/Seg01sc3",
     # "/mnt/data/HW7Leslie/Seg01/Seg01sc4",
-    # Seg02
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc1",
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc2",
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc3",
-    "/mnt/data/HW7Leslie/Seg02/Seg02sc4",
+    # # Seg02
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc1",
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc2",
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc3",
+    # "/mnt/data/HW7Leslie/Seg02/Seg02sc4",
     # # Seg03
     # "/mnt/data/HW7Leslie/Seg03/Seg03sc1",
     # "/mnt/data/HW7Leslie/Seg03/Seg03sc2",
@@ -163,13 +171,13 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     # os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --HomographyGUI --Frame=1")
 
     ########################################################
-    # 1.5 visualizing the region of interest 
+    # 2. visualizing the region of interest 
     # os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --VisROI")
     #######################################################
-    os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --VisROI")
+    # os.system(f"python3 main.py --Dataset={src}  --Detector=Null --Tracker=Null --VisROI")
 
     ########################################################
-    # 2. run the detection
+    # 3. run the detection
     # the full commonad looks like : os.system(f"python3 main.py --Datas`et={src}  --Detector={det} --Tracker=NULL --Detect --DetPostProc --DetMask --DetTh=0.50 --VisDetect")
     ########################################################
     # for det in detectors:
@@ -177,7 +185,7 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     #     os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker=NULL  --VisDetect --DetPostProc --DetTh=0.5")
 
     ########################################################
-    # 3. run the tracking 
+    # 4. run the tracking 
     # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Track --VisTrack --ForNFrames=1800 --Homography --Meter --VisTrajectories --VisTrackTop")
     ########################################################
     # for det in detectors:
@@ -186,7 +194,7 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     #         os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --VisTrack --Homography --Meter --VisTrajectories --VisTrackTop")
 
     ########################################################
-    # 3.1 run the track post processing
+    # 5. run the track post processing
     # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --TrackPostProc --TrackTh=8 --RemoveInvalidTracks --SelectDifEdgeInROI --SelectEndingInROI --SelectBeginInROI --MaskGPFrame --HasPointsInROI --MaskROI  --CrossROI --CrossROIMulti --JustEnterROI --JustExitROI --WithinROI --Interpolate --ExitOrCrossROI")
     ########################################################
     # for det in detectors:
@@ -196,7 +204,7 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
 
 
     ########################################################
-    # 3.1 find optimum BW for kde fiting
+    # 6. find optimum BW for kde fiting
     # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --FindOptimalKDEBW --ResampleTH=2.0")
     ########################################################
     # for det in detectors:
@@ -206,7 +214,7 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
 
 
     ########################################################
-    # 4. run clustering algorithm
+    # 7. run clustering algorithm
     # apperently the clustering visulaizaiton is harcodded at the moment
     ########################################################
     # for det in detectors:
@@ -217,14 +225,14 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     #                 os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --ClusteringAlgo={clt} --ClusterMetric={met} --Cluster")
     
     ########################################################
-    # 5. Run the track labelling GUI / go to 6.
+    # 8. Run the track labelling GUI / go to 6.
     ########################################################
     # for det in detectors:
     #     for tra in trackers:
     #         os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --TrackLabelingGUI --VisLabelledTrajectories")
 
     ########################################################
-    # 6. Run automated track extraction and labelling
+    # 9. Run automated track extraction and labelling
     ########################################################
     # for det in detectors:
     #     for tra in trackers:
@@ -232,7 +240,7 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     #         os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --ExtractCommonTracks --VisLabelledTrajectories --ResampleTH=2.0")
 
     ########################################################
-    # 7. Run the classification(counting) part
+    # 10. Run the classification(counting) part
     # os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Count --CountMetric={metric} --CountVisPrompt --EvalCount --UseCachedCounter --CachedCounterPth={cached_cnt_pth} --CacheCounter --CountVisDensity")
     ########################################################
     # for det in detectors:
@@ -242,7 +250,7 @@ for src, cached_cnt_pth in zip(sources, cached_cnt_sources):
     #             os.system(f"python3 main.py --Dataset={src}  --Detector={det} --Tracker={tra} --Count --CountMetric={metric} --EvalCount --CountVisDensity --CacheCounter")
 
     ########################################################
-    # 8. Visualizing the results on a video including track label and track id
+    # 11. Visualizing the results on a video including track label and track id
     # can be used to monitor the pipeline in detail
     ########################################################
     # for det in detectors:
