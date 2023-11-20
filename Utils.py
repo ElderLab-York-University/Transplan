1f372e8c5ab7285bfc7b4887ffe962ece7b6fb48
sajjadpsavoji <sj.pakdaman@gmail.com>
2023-11-20 15:09:52 -0500

    add sahi as param to detector after version

--- Utils.py
+++ Utils.py
@@ -584,6 +584,10 @@ def adjust_args_with_params(args):
     if not args.DetectorVersion == "":
         args.Detector += "." + args.DetectorVersion
 
+    if args.SAHI:
+        args.Detector += ".SAHI-" + args.SahiPatchSize + "-" +\
+            args.SahiPatchOverlapRatio + "-" + args.SahiNMSTh
+
     return args
 
 def revert_args_with_params(args):
@@ -592,6 +596,10 @@ def revert_args_with_params(args):
 
     if not args.DetectorVersion == "":
         args.Detector  = args.Detector.split(".")[0]
+
+    if args.SAHI:
+        args.Detector  = args.Detector.split(".")[0]
+        
     return args
 
 def get_check_point_dir(args):
