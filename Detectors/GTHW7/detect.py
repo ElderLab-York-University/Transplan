22b031b6380f56d19a2296b277db092402d1903e
sajjadpsavoji <sj.pakdaman@gmail.com>
2024-01-17 12:58:46 -0500

    modify adding uuid to GT detections

--- Detectors/GTHW7/detect.py
+++ Detectors/GTHW7/detect.py
@@ -58,19 +58,21 @@ def detect(args,*oargs):
 def df(args):
   file_path = args.DetectionDetectorPath
   data = {}
-  data["fn"], data["class"], data["score"], data["x1"], data["y1"], data["x2"], data["y2"] = [], [], [], [], [], [], []
+  data["fn"], data["class"], data["score"], data["x1"], data["y1"], data["x2"], data["y2"], data["uuid"] = [], [], [], [], [], [], [], []
   with open(file_path, "r+") as f:
     lines = f.readlines()
     for line in lines:
       splits = line.split(",")
-      fn , clss, score, x1, y1, x2, y2 = float(splits[0]), float(splits[1]), float(splits[2]), float(splits[3]), float(splits[4]), float(splits[5]), float(splits[6])
-      data["fn"].append(fn)
+      fn , clss, score, x1, y1, x2, y2, uuid = float(splits[0]), float(splits[1]), float(splits[2]), float(splits[3]),\
+                                               float(splits[4]), float(splits[5]), float(splits[6]), str(splits[7])
+      data["fn"   ].append(fn)
       data["class"].append(clss)
       data["score"].append(score)
-      data["x1"].append(x1)
-      data["y1"].append(y1)
-      data["x2"].append(x2)
-      data["y2"].append(y2)
+      data["x1"   ].append(x1)
+      data["y1"   ].append(y1)
+      data["x2"   ].append(x2)
+      data["y2"   ].append(y2)
+      data["uuid" ].append(uuid)
   return pd.DataFrame.from_dict(data)
 
 def df_txt(df,text_result_path):
@@ -82,5 +84,5 @@ def df_txt(df,text_result_path):
 
   with open(text_result_path, "w") as text_file:
     for i, row in tqdm(df.iterrows()):
-      frame_num, clss, score, x1, y1, x2, y2 = row["fn"], row['class'], row["score"], row["x1"], row["y1"], row["x2"], row["y2"]
-      text_file.write(f"{frame_num},{clss},{score},{x1},{y1},{x2},{y2}\n")
+      frame_num, clss, score, x1, y1, x2, y2, uuid = row["fn"], row['class'], row["score"], row["x1"], row["y1"], row["x2"], row["y2"], row["uuid"]
+      text_file.write(f"{frame_num},{clss},{score},{x1},{y1},{x2},{y2},{uuid}\n")
