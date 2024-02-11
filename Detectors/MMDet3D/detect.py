ad129c72fa1ec301a49f3cff1ffc04c22a522608
sajjadpsavoji <sj.pakdaman@gmail.com>
2024-02-11 12:47:18 -0500

    adding detect.py and run.py templates. detect setup completed

---
+++ Detectors/MMDet3D/detect.py
@@ -0,0 +1,134 @@
+from Libs import *
+from Utils import *
+
+def detect(args):
+    setup(args)
+    # this detector is an abstract detector
+    # it plays as a wraper for all detectors under mmdet
+
+def setup(args):
+    env_name = "MMDet3D"
+    src_url = "https://github.com/open-mmlab/mmdetection3d"
+    rep_path = "./Detectors/MMDet3D/mmdetection3d"
+
+    if not "mmdetection3d" in os.listdir("./Detectors/MMDet3D/"):
+        os.system(f"git clone {src_url} {rep_path}")
+
+    if not env_name in get_conda_envs():
+        initial_directory = os.getcwd()
+        make_conda_env(env_name, libs="python=3.8")
+        # make sure pip is installed
+        os.system(f"conda run -n {env_name} --live-stream conda install pip")
+        # install pytorch 1.8+
+        os.system(f"conda run -n {env_name} --live-stream conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch -y")
+        os.system(f"conda run -n {env_name} --live-stream conda install -c nvidia cuda-nvcc=11.3 -y")  
+        # install mm dependancy
+        os.system(f"conda run -n {env_name} --live-stream pip install -U openmim")
+        os.system(f"conda run -n {env_name} --live-stream mim install mmengine")
+        os.system(f"conda run -n {env_name} --live-stream mim install 'mmcv>=2.0.0rc4'")
+        os.system(f"conda run -n {env_name} --live-stream mim install 'mmdet>=3.0.0'")
+        # install mmdet3D from source code
+        os.chdir(rep_path)
+        os.system(f"conda run -n {env_name} --live-stream pip install -v -e .")
+        # verify correct installation
+        os.system(f"conda run -n {env_name} --live-stream mim download mmdet3d --config pointpillars_hv_secfpn_8xb6-160e_kitti-3d-car --dest .")
+        os.system(f"conda run -n {env_name} --live-stream python demo/pcd_demo.py demo/data/kitti/000008.bin pointpillars_hv_secfpn_8xb6-160e_kitti-3d-car.py hv_pointpillars_secfpn_6x8_160e_kitti-3d-car_20220331_134606-d42d15ed.pth --show")
+
+        os.chdir(initial_directory)
+
+def df(args):
+  file_path = args.DetectionDetectorPath
+  data = {}
+  data["fn"], data["class"], data["score"], data["x1"], data["y1"], data["x2"], data["y2"] = [], [], [], [], [], [], []
+  with open(file_path, "r+") as f:
+    lines = f.readlines()
+    for line in lines:
+      splits = line.split()
+      fn , clss, score, x1, y1, x2, y2 = float(splits[0]), float(splits[1]), float(splits[2]), float(splits[3]), float(splits[4]), float(splits[5]), float(splits[6])
+      data["fn"].append(fn)
+      data["class"].append(clss)
+      data["score"].append(score)
+      data["x1"].append(x1)
+      data["y1"].append(y1)
+      data["x2"].append(x2)
+      data["y2"].append(y2)
+  return pd.DataFrame.from_dict(data)
+
+def df_txt(df,text_result_path):
+  # store a modified version of detection df to the same txt file
+  # used in the post processig part of the detection
+  # df is in the same format specified in the df function
+  with open(text_result_path, "w") as text_file:
+    pass
+
+  with open(text_result_path, "w") as text_file:
+    for i, row in tqdm(df.iterrows()):
+      frame_num, clss, score, x1, y1, x2, y2 = row["fn"], row['class'], row["score"], row["x1"], row["y1"], row["x2"], row["y2"]
+      text_file.write(f"{frame_num} {clss} {score} {x1} {y1} {x2} {y2}\n")
+
+def fine_tune(train_config_path, work_dir, resume):
+  env_name = "MMDet "
+  ngpus = get_numgpus_torch(env_name)
+  port  = get_available_port()
+  if resume:
+    os.system(f"conda run -n {env_name} --live-stream PORT={port} ./Detectors/MMDet/mmdetection/tools/dist_train.sh {train_config_path} {ngpus} --work-dir={work_dir} --resume")
+  else:
+    os.system(f"conda run -n {env_name} --live-stream PORT={port} ./Detectors/MMDet/mmdetection/tools/dist_train.sh {train_config_path} {ngpus} --work-dir={work_dir}")
+
+def modify_train_config(train_config_path, args, args_mp, args_gt, args_mp_gt):
+  file_name = train_config_path
+  root_dataset = os.path.abspath(args_gt.Dataset)
+  for arg_p in args_mp_gt:
+    if arg_p.SubID == args_gt.TrainPart:
+      arg_p_train = arg_p
+    if arg_p.SubID == args_gt.ValidPart:
+      arg_p_valid = arg_p
+
+  train_ann_file = os.path.relpath(arg_p_train.DetectionCOCO, root_dataset)
+  valid_ann_file = os.path.relpath(arg_p_valid.DetectionCOCO, root_dataset)
+
+  train_data_prefix = arg_p_train.SubID
+  valid_data_prefix = arg_p_valid.SubID
+
+  with open(arg_p_train.DetectionCOCO, "r") as f:
+    ann = json.load(f)
+    num_classes = len(ann["categories"])
+    classes = tuple([d["name"] for d in ann["categories"]])  
+
+  # create the new content
+  NewLine = "\n"
+  new_content = ""
+  new_content += f"data_root = '{root_dataset}/'" + NewLine
+  new_content += f"train_ann_file = '{train_ann_file}'" + NewLine
+  new_content += f"train_data_prefix = '{train_data_prefix}/'" + NewLine
+  new_content += f"valid_ann_file = '{valid_ann_file}'" + NewLine
+  new_content += f"valid_data_prefix = '{valid_data_prefix}/'" + NewLine
+  new_content += f"BatchSize = {args_gt.BatchSize}" + NewLine
+  new_content += f"NumWorkers = {args_gt.NumWorkers}" + NewLine
+  new_content += f"Epochs = {args_gt.Epochs}" + NewLine
+  new_content += f"ValInterval = {args_gt.ValInterval}" + NewLine
+  new_content += f"num_classes = {num_classes}" + NewLine
+  new_content += f"classes = {classes}" + NewLine
+
+  # Read the content of the file
+  with open(file_name, 'r') as file:
+      lines = file.readlines()
+
+  # Find the indices of the comment lines that mark the section to change
+  start_index = None
+  end_index = None
+  for i, line in enumerate(lines):
+      if line.strip() == "#CHANGE#BELOW#":
+          start_index = i + 1
+      elif line.strip() == "#CHANGE#ABOVE#":
+          end_index = i
+          break
+
+  # Modify the content between the comment lines
+  new_lines = lines[:start_index]
+  new_lines += new_content
+  new_lines += lines[end_index:]
+
+  # Write the modified content back to the file
+  with open(file_name, 'w') as file:
+      file.writelines(new_lines)
---
+++ Detectors/MMDet3D/run.py
@@ -0,0 +1,224 @@
+import cv2
+import mmcv
+from mmcv.transforms import Compose
+from mmengine.utils import track_iter_progress
+from mmdet.registry import VISUALIZERS
+from mmdet.apis import init_detector, inference_detector
+import json
+from tqdm import tqdm
+import sys
+import torch
+import mmcv
+from sahi.slicing import slice_image
+
+# Nov 20 2023 NOTE
+# as of today SAHI does not support bbox with negatiev coordinate
+# while mmdet might output such bboxes in the model
+# to merge it with sahi there are two options
+# 1. clip bbox to be olways positive
+# 2. modify sahi to accept negative bbox values
+# I believe second option is better as it preserves more info about
+# detected bonding boxes
+# for that reason we will temporary implement the bbox shifring locally
+# and we will merge to late releases of SAHI or MMDet
+from typing import Sequence, Tuple
+import torch
+from mmcv.ops import batched_nms
+from mmengine.structures import InstanceData
+from mmdet.structures import DetDataSample, SampleList
+def merge_results_by_nms(results: SampleList, offsets: Sequence[Tuple[int,
+                                                                      int]],
+                         src_image_shape: Tuple[int, int],
+                         nms_cfg: dict) -> DetDataSample:
+    """Merge patch results by nms.
+
+    Args:
+        results (List[:obj:`DetDataSample`]): A list of patch results.
+        offsets (Sequence[Tuple[int, int]]): Positions of the left top points
+            of patches.
+        src_image_shape (Tuple[int, int]): A (height, width) tuple of the large
+            image's width and height.
+        nms_cfg (dict): it should specify nms type and other parameters
+            like `iou_threshold`.
+    Returns:
+        :obj:`DetDataSample`: merged results.
+    """
+    shifted_instances = shift_predictions(results, offsets, src_image_shape)
+
+    _, keeps = batched_nms(
+        boxes=shifted_instances.bboxes,
+        scores=shifted_instances.scores,
+        idxs=shifted_instances.labels,
+        nms_cfg=nms_cfg)
+    merged_instances = shifted_instances[keeps]
+
+    merged_result = results[0].clone()
+    merged_result.pred_instances = merged_instances
+    return merged_result
+
+def shift_predictions(det_data_samples: SampleList,
+                      offsets: Sequence[Tuple[int, int]],
+                      src_image_shape: Tuple[int, int]) -> SampleList:
+    """Shift predictions to the original image.
+
+    Args:
+        det_data_samples (List[:obj:`DetDataSample`]): A list of patch results.
+        offsets (Sequence[Tuple[int, int]]): Positions of the left top points
+            of patches.
+        src_image_shape (Tuple[int, int]): A (height, width) tuple of the large
+            image's width and height.
+    Returns:
+        (List[:obj:`DetDataSample`]): shifted results.
+    """
+    
+    assert len(det_data_samples) == len(
+        offsets), 'The `results` should has the ' 'same length with `offsets`.'
+    shifted_predictions = []
+    for det_data_sample, offset in zip(det_data_samples, offsets):
+        pred_inst = det_data_sample.pred_instances.clone()
+
+        # Check bbox type
+        if pred_inst.bboxes.size(-1) == 4:
+            # Horizontal bboxes
+            shifted_bboxes = shift_bboxes(pred_inst.bboxes, offset)
+        elif pred_inst.bboxes.size(-1) == 5:
+            # Rotated bboxes
+            shifted_bboxes = shift_rbboxes(pred_inst.bboxes, offset)
+        else:
+            raise NotImplementedError
+
+        # shift bboxes and masks
+        pred_inst.bboxes = shifted_bboxes
+        if 'masks' in det_data_sample:
+            pred_inst.masks = shift_masks(pred_inst.masks, offset,
+                                          src_image_shape)
+
+        shifted_predictions.append(pred_inst.clone())
+
+    shifted_predictions = InstanceData.cat(shifted_predictions)
+
+    return shifted_predictions
+
+def shift_rbboxes(bboxes: torch.Tensor, offset: Sequence[int]):
+    """Shift rotated bboxes with offset.
+
+    Args:
+        bboxes (Tensor): The rotated bboxes need to be translated.
+            With shape (n, 5), which means (x, y, w, h, a).
+        offset (Sequence[int]): The translation offsets with shape of (2, ).
+    Returns:
+        Tensor: Shifted rotated bboxes.
+    """
+    offset_tensor = bboxes.new_tensor(offset)
+    shifted_bboxes = bboxes.clone()
+    shifted_bboxes[:, 0:2] = shifted_bboxes[:, 0:2] + offset_tensor
+    return shifted_bboxes
+
+def shift_bboxes(bboxes: torch.Tensor, offset: Sequence[int]):
+    """Shift bboxes with offset.
+
+    Args:
+        bboxes (Tensor): The bboxes need to be translated.
+            With shape (n, 4), which means (x, y, x, y).
+        offset (Sequence[int]): The translation offsets with shape of (2, ).
+    Returns:
+        Tensor: Shifted rotated bboxes.
+    """
+    offset_tensor = bboxes.new_tensor(offset)
+    shifted_bboxes = bboxes.clone()
+    shifted_bboxes[:, 0:2] = shifted_bboxes[:, 0:2] + offset_tensor
+    shifted_bboxes[:, 2:4] = shifted_bboxes[:, 2:4] + offset_tensor
+    return shifted_bboxes
+
+# actual code begis
+def preds_from_result(result):
+    if 'pred_instances' in result:
+        instances = result.pred_instances
+        scores = instances.scores
+        bboxes = instances.bboxes # x1, y1, x2, y2
+        labels = instances.labels
+    return bboxes, labels, scores
+
+def get_sahi_result(frame, model, test_pipeline, args):
+    height, width = frame.shape[:2]
+    sliced_image_object = slice_image(
+        frame,
+        slice_height=args["SahiPatchSize"],
+        slice_width=args["SahiPatchSize"],
+        auto_slice_resolution=False,
+        overlap_height_ratio=args["SahiPatchOverlapRatio"],
+        overlap_width_ratio=args["SahiPatchOverlapRatio"],
+    )
+
+    # if patch batch is not specified, use all the patches all the time
+    if args["SahiPatchBatchSize"]:
+        SahiPatchBatchSize = args["SahiPatchBatchSize"]
+    else:
+        SahiPatchBatchSize = len(sliced_image_object)
+
+    slice_results = []
+    start = 0
+    while True:
+        # prepare batch slices
+        end = min(start + SahiPatchBatchSize, len(sliced_image_object))
+        images = []
+        for sliced_image in sliced_image_object.images[start:end]:
+            images.append(sliced_image)
+
+        # forward the model
+        slice_results.extend(inference_detector(model, images, test_pipeline=test_pipeline))
+
+        if end >= len(sliced_image_object):
+            break
+        start += SahiPatchBatchSize
+
+    image_result = merge_results_by_nms(
+    slice_results,
+    sliced_image_object.starting_pixels,
+    src_image_shape=(height, width),
+    nms_cfg={
+        'type': 'nms',
+        'iou_threshold': args["SahiNMSTh"]
+    })
+
+    result = image_result.cpu()
+    return result
+
+def get_full_frame_result(frame, model, test_pipeline, args):
+    result = inference_detector(model, frame, test_pipeline=test_pipeline)
+    result = result.cpu()
+    return result
+
+if __name__ == "__main__":
+    args = json.loads(sys.argv[-1])
+    # config and check point will come from args
+    config_file = args["MMDetConfig"]
+    checkpoint_file = args["MMDetCheckPoint"]
+    # select compute device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device_name = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(f'device: {device_name}')
+    # set video and result path
+    video_path = args["Video"]
+    text_result_path = args["DetectionDetectorPath"] 
+
+    # Build the model from a config file and a checkpoint file
+    model = init_detector(config_file, checkpoint_file, device=device_name)
+
+    model.cfg.test_dataloader.dataset.pipeline[0].type = 'LoadImageFromNDArray'
+    test_pipeline = Compose(model.cfg.test_dataloader.dataset.pipeline)
+
+    video_reader = mmcv.VideoReader(video_path)
+    fn=0
+    with open (text_result_path,"w") as f: 
+        for frame in tqdm(video_reader):
+            if args["SAHI"]:
+                result = get_sahi_result(frame, model, test_pipeline, args)
+            else:
+                result = get_full_frame_result(frame, model, test_pipeline, args)
+
+            bboxes, labels, scores = preds_from_result(result)
+            for box, label, score in zip(bboxes,labels, scores):
+                    r=box
+                    f.write(f"{fn} {label} {score} {box[0]} {box[1]} {box[2]} {box[3]}\n")
+            fn+=1
