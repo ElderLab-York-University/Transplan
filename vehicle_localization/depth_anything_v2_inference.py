b88db924871176fd91f084bbe72e75efdddaa659
MoHoss007 <mmhossirca@gmail.com>
2024-09-05 10:45:38 -0400

    Added batch inference to depth anything inference class

--- vehicle_localization/depth_anything_v2_inference.py
+++ vehicle_localization/depth_anything_v2_inference.py
@@ -3,6 +3,7 @@ from PIL import Image
 import requests
 import cv2
 import numpy as np
+from typing import List
 
 
 class DepthAnythingV2Inference:
@@ -24,3 +25,18 @@ class DepthAnythingV2Inference:
         depth = self.pipe(pil_image)["depth"]
 
         return np.array(depth)
+
+    def run_batch_inference(self, cv2_imgs: List[np.ndarray]) -> list:
+        rgb_images = [
+            cv2.cvtColor(
+                cv2_img,
+                cv2.COLOR_BGR2RGB,
+            )
+            for cv2_img in cv2_imgs
+        ]
+
+        pil_images = [Image.fromarray(rgb_image) for rgb_image in rgb_images]
+
+        depth = self.pipe(pil_images)
+
+        return depth
